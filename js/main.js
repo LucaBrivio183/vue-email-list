cd380c0dd412a2a4dbdd938d2d0f888470011289
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-21 15:54:00 +0100

    feat:  excercise  done

--- js/main.js
+++ js/main.js
@@ -3,9 +3,19 @@ const  { createApp } = Vue; //initialize Vue
 createApp ({
     data() {  //Vue section for data
         return {
-        },
+            rngEmailList: []
+        }
     },
     methods() { //Vue section for function
 
     },
+    beforeMount(){
+        for(let i=0; i<=10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+            this.rngEmailList.push(response.data);
+            });
+        }       
+    },
 }).mount('#app');
+
