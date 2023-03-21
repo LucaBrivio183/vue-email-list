332c23e7b4173f14eaafea12d8d3e079f4534dc7
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-21 16:31:13 +0100

    feat: style and bonus

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const  { createApp } = Vue; //initialize Vue
 createApp ({
     data() {  //Vue section for data
         return {
-            rngEmailList: []
+            rngEmailList: [],
+            show: false,
         }
     },
     methods() { //Vue section for function
@@ -12,10 +13,11 @@ createApp ({
     beforeMount(){
         for(let i=0; i<=10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) => {
-            this.rngEmailList.push(response.data);
+                .then((response) => {
+                this.rngEmailList.push(response.data);
+                if  (i === 10) {this.show = true}
             });
-        }       
+        }
     },
 }).mount('#app');
 
